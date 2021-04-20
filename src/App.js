f693e17c2516a605490c0563b6ac5fbe2f043e23
MAYANKSHUKLA123 <ramkumarshukla719@gmail.com>
2021-04-21 00:13:46 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import Provider from "./provider";
+import Context from "./context";
 
-function App() {
+const AgentTwo = () => {
+  return <AgentBond />;
+};
+const Agent = () => {
+  return <AgentTwo />;
+};
+// const AgentBond = () => {
+//   return (
+//     <Context.Consumer>
+//       {(context) => (
+//         <Fragment>
+//           <h3>Agent Information</h3>
+//           <p>Mission name: {context.data.mname}</p>
+//         </Fragment>
+//       )}
+//     </Context.Consumer>
+//   );
+// };
+const AgentBond = () => {
+  return (
+    <Context.Consumer>
+      {(context) => (
+        <Fragment>
+          <h1>Mission name</h1>
+          <p>Agent Information: {context.data.mname}</p>
+        </Fragment>
+      )}
+    </Context.Consumer>
+  );
+};
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Context API</h1>
+        <Provider>
+          <Agent />
+        </Provider>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
